d7a0377ed1db7102caf7080114729c1656a0d74c
ndongo78 <ndongodahs@yahoo.fr>
2023-10-02 20:04:32 +0200

    adr video call

--- src/app/(signUp)/signUp/page.tsx
+++ src/app/(signUp)/signUp/page.tsx
@@ -1,14 +1,19 @@
 "use client"
 import Link  from "next/link";
 import "@/styles/login.scss";
+import {AiFillWechat} from "react-icons/ai";
 
 const Register = () => {
     const handleSubmit = () => {}
     return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                <h2 className="logo">Mon logo </h2>
-                <h3 className="title">Se connecter</h3>
+        <div className="formContainer ">
+            <div className="formWrapper h-screen">
+                <AiFillWechat
+                    size={250}
+                    color="lightgreen"
+                    className=" bg-white rounded-3xl p-1"
+                />
+                <h3 className="title italic m-2">Rejoindre la communauté</h3>
                 <form onSubmit={handleSubmit}>
                     <div className="form-control">
                         <label htmlFor="">Votre Nom et Prénom</label>
@@ -26,7 +31,7 @@ const Register = () => {
                         <label htmlFor="">Votre numéro de téléphone</label>
                         <input type="tel" placeholder="telephone" />
                     </div>
-                    <button>Sign in</button>
+                    <button>S&apos;inscrire</button>
                     {/* {err && <span>Something went wrong</span>} */}
                 </form>
                 <p>Vous avez déja un compte? <Link href={"/"} className='span' >Se connecter</Link></p>
